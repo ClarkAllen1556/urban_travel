47e309553c4d2290b5fcbe1c4dc0a0a9d0a13f4d
AC1556 <allenclark@u.boisestate.edu>
2022-06-15 11:26:26 +0900

    Testing build pipeline

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <h1> Testing </h1>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
