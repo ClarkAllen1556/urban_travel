3343bff9c18db78c975a3a45984300786b7f44be
AC1556 <allenclark@u.boisestate.edu>
2022-06-15 14:17:24 +0900

    adding in static assets

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue';
-</script>
+<script setup></script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-
-  <div>
-    {{ $t('message.hello') }}
-  </div>
+  <img alt="CloudTravel logo" src="./assets/logo.jpg" />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
